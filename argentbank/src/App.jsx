f86af575ddbc83aa39494989c3370ba7d8fc46da
Amandine Arias <amandine.arias@gmail.com>
2021-11-02 14:53:32 +0100

    update (App) : update & add Routes

--- argentbank/src/App.jsx
+++ argentbank/src/App.jsx
@@ -1,22 +1,31 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import Transactions from './pages/Transactions';
+import User from './pages/User';
 import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="*" render={() => <p>Error 404</p>} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/transactions" component={Transactions} />
+        <Route path="/user" component={User} />
+        <Route
+          path="*"
+          render={() => (
+            <p className="error-msg">
+              Page not found <br /> Error 404
+            </p>
+          )}
+        />
       </Switch>
       <Footer />
     </BrowserRouter>
   );
 }
-
-export default App;
