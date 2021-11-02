32bd7815077781043f7618f8b67d04e99c603b1e
Amandine Arias <amandine.arias@gmail.com>
2021-11-02 14:28:50 +0100

    feat (SignIn) : create component

--- argentbank/src/pages/SignIn.jsx
+++ argentbank/src/pages/SignIn.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import NavbarIn from '../components/NavbarIn';
 
 export default function SignIn() {
-  return <h1>sign in</h1>;
+  return (
+    <>
+      <NavbarIn />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <htmlForm>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <Link to="/profile" className="sign-in-button">
+              <button className="sign-in-button">Sign In</button>
+            </Link>
+          </htmlForm>
+        </section>
+      </main>
+    </>
+  );
 }
