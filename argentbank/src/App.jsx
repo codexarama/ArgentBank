a741fd8cbcd5128243b5501e2285be813736fa7f
Amandine Arias <amandine.arias@gmail.com>
2021-11-09 15:32:07 +0100

    refactor (App) : remove useless routes

--- argentbank/src/App.jsx
+++ argentbank/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
-import Transactions from './pages/Transactions';
-import User from './pages/User';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -14,8 +12,6 @@ export default function App() {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={SignIn} />
         <Route path="/profile" component={Profile} />
-        <Route path="/transactions" component={Transactions} />
-        <Route path="/user" component={User} />
         <Route
           path="*"
           render={() => (
