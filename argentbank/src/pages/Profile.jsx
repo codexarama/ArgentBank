54e009d2f1829757d5baae77afe2d1b8325fabb7
Amandine Arias <amandine.arias@gmail.com>
2021-11-19 13:25:48 +0100

    fix (Profile) : manage UpdateProfile form visibility state (managed in the component too)

--- argentbank/src/pages/Profile.jsx
+++ argentbank/src/pages/Profile.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 import Welcome from '../components/Welcome';
-import User from '../components/User';
+import User from '../components/UpdateProfile';
 import { accountData } from '../services/data';
 import Account from '../components/Account';
+// import { Redirect } from 'react-router';
 
 export default function Profile() {
   useEffect(() => {
-    document.title = "Argent Bank | Profile";
+    document.title = 'Argent Bank | Profile';
   }, []);
 
-  const [changeProfile, setChangeProfile] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  // const [changeProfile, setChangeProfile] = useState(false);
 
   const user = localStorage.getItem('USER');
 
   return (
     <main className="profile bg-dark">
-      {changeProfile ? (<User />
+      {showForm ? (
+        <User />
+        // <Redirect to="profile/update" />
       ) : (
         <>
           <Welcome fullName={user} />
@@ -24,7 +28,8 @@ export default function Profile() {
             className="edit-button"
             type="submit"
             value="Edit Name"
-            onClick={() => setChangeProfile(true)}
+            onClick={() => setShowForm(true)}
+            // onClick={() => setChangeProfile(true)}
           />
           <h2 className="sr-only">Accounts</h2>
           {accountData.map((item) => (
