caaad8e13c700cadc41a63212b6922596b9c4938
Amandine Arias <amandine.arias@gmail.com>
2021-11-19 11:52:22 +0100

    fix (edit profile) : custom views & form interactions

--- argentbank/src/components/Welcome.jsx
+++ argentbank/src/components/Welcome.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import User from './User';
-
 export default function Welcome({ fullName }) {
-  const [changeProfile, setChangeProfile] = useState(false);
-
   return (
-    <div className="header">
+    <header className="header">
       <h1>
         Welcome back
         <br />
         {fullName}!
       </h1>
-      {changeProfile ? (
-      <User />
-    ) : (
-      <input
-        className="edit-button"
-        type="submit"
-        value="Edit Name"
-        onClick={() => setChangeProfile(true)}
-      />
-    )}
-    </div>
+    </header>
   );
 }
--- argentbank/src/pages/Profile.jsx
+++ argentbank/src/pages/Profile.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Welcome from '../components/Welcome';
-
+import User from '../components/User';
 import { accountData } from '../services/data';
 import Account from '../components/Account';
 
 export default function Profile() {
+  useEffect(() => {
+    document.title = "Argent Bank | Profile";
+  }, []);
+
+  const [changeProfile, setChangeProfile] = useState(false);
+
   const user = localStorage.getItem('USER');
 
   return (
-      <main className="profile bg-dark">
-        <Welcome fullName={user} />
-        <h2 className="sr-only">Accounts</h2>
-        {accountData.map((item) => (
-          <Account
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            description={item.description}
+    <main className="profile bg-dark">
+      {changeProfile ? (<User />
+      ) : (
+        <>
+          <Welcome fullName={user} />
+          <input
+            className="edit-button"
+            type="submit"
+            value="Edit Name"
+            onClick={() => setChangeProfile(true)}
           />
-        ))}
-      </main>
+          <h2 className="sr-only">Accounts</h2>
+          {accountData.map((item) => (
+            <Account
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              description={item.description}
+            />
+          ))}
+        </>
+      )}
+    </main>
   );
 }
