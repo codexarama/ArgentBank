e948ac8460fd21baeae044c865fe90b7c52658f4
Amandine Arias <amandine.arias@gmail.com>
2021-11-23 11:53:58 +0100

    work in progress : EDIT & CHANGE PROFILE

--- argentbank/src/_store/middlewares/userMiddleware.js
+++ argentbank/src/_store/middlewares/userMiddleware.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { getUser, editProfile } from '../actions/userActions';
+import { editSuccess, editFailure } from '../actions/userActions';
 import {
-  getValueFromLocalStorage,
-  setValueToLocalStorage,
-} from '../../utils/localStorage';
+  getValueFromSessionStorage,
+  setValueToSessionStorage,
+} from '../../utils/sessionStorage';
 
 const baseURL = 'http://localhost:3001/api/v1/user';
 // const baseURL = `${process.env.REACT_APP_API_URL}/api/v1/user`;
@@ -14,17 +14,24 @@ const baseURL = 'http://localhost:3001/api/v1/user';
  * @param   {object}  data      [user data]
  * @returns {object}  response  [data || undefined || error]
  */
-export function userData() {
+export function getUser() {
+  let token = getValueFromSessionStorage('TOKEN');
+  let user = getValueFromSessionStorage('USER');
+  console.log(token, user);
   return (dispatch) => {
-    let token = getValueFromLocalStorage('TOKEN');
     axios
-      .post(`${baseURL}/profile`, {
+      .post(`${baseURL}/profile`,
+      {},
+      {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        dispatch(getUser(response.data.body));
+        dispatch(editSuccess(token, user));
+        // dispatch(editSuccess(response.data.body.user.firstName, response.data.body.user.lastName));
       })
-      .catch((error) => console.error(error.message));
+      .catch((error) => {
+        dispatch(editFailure(error.message));
+      });
   };
 }
 
@@ -34,25 +41,119 @@ export function userData() {
  * @param   {string}  lastName    [new user lastName]
  * @returns
  */
- export function newProfile(firstName, lastName) {
-  let token = getValueFromLocalStorage('TOKEN');
+export function newProfile(firstName, lastName, token) {
   return (dispatch) => {
     axios
       .put(
         `${baseURL}/profile`,
-        { headers: { Authorization: `Bearer ${token}` } },
-        { firstName, lastName }
+        { firstName, lastName },
+        { headers: { Authorization: `Bearer ${token}` } }
       )
       .then((response) => {
-        dispatch(editProfile(response.data.body));
-        setValueToLocalStorage(
+        dispatch(editSuccess(firstName, lastName));
+        setValueToSessionStorage(
           'USER',
           response.data.body.user.firstName +
             ' ' +
             response.data.body.user.lastName
         );
-        // window.location.replace(`/profile`);
       })
-      .catch((error) => console.error(error.message));
+      .catch((error) => {
+        dispatch(editFailure(error.message));
+      });
   };
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+// import axios from 'axios';
+// import { editSuccess, editFailure } from '../actions/userActions';
+// import {
+//   getValueFromLocalStorage,
+//   setValueToLocalStorage,
+// } from '../../utils/localStorage';
+
+// const baseURL = 'http://localhost:3001/api/v1/user';
+// // const baseURL = `${process.env.REACT_APP_API_URL}/api/v1/user`;
+
+// /**
+//  * GET USER DATA
+//  * fetch user data
+//  * @param   {object}  data      [user data]
+//  * @returns {object}  response  [data || undefined || error]
+//  */
+// export function getUser() {
+//   return (dispatch) => {
+//     let token = getValueFromLocalStorage('TOKEN');
+//     axios
+//       .post(`${baseURL}/profile`, {
+//         headers: { Authorization: `Bearer ${token}` },
+//       })
+//       .then((response) => {
+//         dispatch(editSuccess(response.data.body));
+//       })
+//       .catch((error) => {
+//         dispatch(editFailure(error.message));
+//       });
+//   };
+// }
+
+// // /**
+// //  * PUT NEW USER NAME in API
+// //  * @param   {string}  firstName   [new user firstName]
+// //  * @param   {string}  lastName    [new user lastName]
+// //  * @returns
+// //  */
+// // export function newProfile(firstName, lastName) {
+// //   let user = getValueFromLocalStorage('USER');
+// //   return (dispatch) => {
+// //     axios
+// //       .put(
+// //         `${baseURL}/profile`,
+// //         { headers: { Authorization: `Bearer ${user}` } },
+// //         { firstName, lastName }
+// //       )
+// //       .then((response) => {
+// //         dispatch(editProfile(response.data.body));
+// //         setValueToLocalStorage(
+// //           'USER',
+// //           response.data.body.user.firstName +
+// //             ' ' +
+// //             response.data.body.user.lastName
+// //         );
+// //       })
+// // .catch((error) => {
+// //   dispatch(editFailure(error.message));
+// // });
+// //   };
+// // }
+
+// /**
+//  * PUT NEW USER NAME in API
+//  * @param   {string}  firstName   [new user firstName]
+//  * @param   {string}  lastName    [new user lastName]
+//  * @returns
+//  */
+// export function newProfile(firstName, lastName) {
+//   let token = getValueFromLocalStorage('TOKEN');
+//   return (dispatch) => {
+//     axios
+//       .put(
+//         `${baseURL}/profile`,
+//         { headers: { Authorization: `Bearer ${token}` } },
+//         { firstName, lastName }
+//       )
+//       .then((response) => {
+//         dispatch(editSuccess(response.data.body));
+//         setValueToLocalStorage(
+//           'USER',
+//           response.data.body.user.firstName +
+//             ' ' +
+//             response.data.body.user.lastName
+//         );
+//         // window.location.replace(`/profile`);
+//       })
+//       .catch((error) => {
+//         dispatch(editFailure(error.message));
+//       });
+//   };
+// }
--- argentbank/src/components/UpdateProfile.jsx
+++ argentbank/src/components/UpdateProfile.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-// import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { newProfile } from '../_store/middlewares/userMiddleware';
+import { getUser, newProfile } from '../_store/middlewares/userMiddleware';
 
 import Profile from '../pages/Profile';
 
@@ -21,15 +21,24 @@ export default function User() {
   const [changeProfile, setChangeProfile] = useState(false);
 
   const dispatch = useDispatch();
-  
-  // const user = useSelector((state) => state.userReducer.user);
-  // console.log(user);
+
+  const user = (state) => state.userReducer;
+  const currentUser = useSelector(user);
+  console.log(currentUser); // null
+
+  useEffect(() => {
+    dispatch(getUser(currentUser.token));
+  }, [dispatch, currentUser.token]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch(newProfile(firstName, lastName));
+    if (firstName && lastName !== '') {
+      dispatch(newProfile(firstName, lastName, currentUser.token));
+      // setFirstName('');
+      // setLastName('');
+    }
     setChangeProfile(true);
-  };
+  }; // erreur 401 (Unauthorized)
 
   return (
     <>
@@ -85,3 +94,90 @@ export default function User() {
     </>
   );
 }
+// import React, { useEffect, useState } from 'react';
+// // import { useDispatch } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
+
+// import { newProfile } from '../_store/middlewares/userMiddleware';
+
+// import Profile from '../pages/Profile';
+
+// /**
+//  *
+//  * @returns
+//  */
+// export default function User() {
+//   useEffect(() => {
+//     document.title = 'Argent Bank | Update Profile';
+//   }, []);
+
+//   const [editProfile, setEditProfile] = useState(true);
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [changeProfile, setChangeProfile] = useState(false);
+
+//   const dispatch = useDispatch();
+
+//   const user = useSelector((state) => state.userReducer.user);
+//   console.log(user); // null
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     dispatch(newProfile(firstName, lastName));
+//     setChangeProfile(true);
+//   }; // erreur 401 (Unauthorized)
+
+//   return (
+//     <>
+//       {editProfile ? (
+//         <section className="update-profile-content">
+//           <i className="fa fa-user-circle update-profile-icon"></i>
+//           <h1>Update profile</h1>
+//           <form action="" onSubmit={handleSubmit} id="update-profile-form">
+//             <div className="input-wrapper">
+//               <label htmlFor="firstName">First name</label>
+//               <input
+//                 type="text"
+//                 id="firstName"
+//                 onChange={(event) => setFirstName(event.target.value)}
+//                 value={firstName}
+//               />
+//               {changeProfile && !firstName && (
+//                 <small className="input-error">First name is required</small>
+//               )}
+//             </div>
+//             <div className="input-wrapper">
+//               <label htmlFor="lastName">Last name</label>
+//               <input
+//                 type="lastName"
+//                 id="lastName"
+//                 onChange={(event) => setLastName(event.target.value)}
+//                 value={lastName}
+//               />
+//               {changeProfile && !lastName && (
+//                 <small className="input-error">Last name is required</small>
+//               )}
+//             </div>
+//             <input
+//               className="update-profile-button"
+//               type="submit"
+//               value="Save"
+//             />
+//             <input
+//               className="cancel-button"
+//               type="button"
+//               value="Cancel"
+//               onClick={() => {
+//                 setFirstName('');
+//                 setLastName('');
+//                 setEditProfile(false);
+//               }}
+//             />
+//           </form>
+//         </section>
+//       ) : (
+//         <Profile />
+//       )}
+//     </>
+//   );
+// }
