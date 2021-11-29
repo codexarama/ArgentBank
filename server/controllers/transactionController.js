0d22edb3957e5f75d828ad7b52f881e26c12bd47
Amandine Arias <amandine.arias@gmail.com>
2021-11-29 17:15:11 +0100

    feat (backend) : transaction controller

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -5,11 +5,63 @@ module.exports.createTransaction = async (req, res) => {
 
   try {
     const responseFromService = await transactionService.createTransaction(req.body)
-    response.status = 200
+    // 201 FOR CREATION ////////////////////////////////////////////////////////////
+    response.status = 201
     response.message = 'Transaction successfully created'
     response.body = responseFromService
   } catch (error) {
     console.error('Something went wrong in transactionController.js', error)
+    response.status = 400
+    response.message = error.message
+  }
+
+  return res.status(response.status).send(response)
+}
+
+module.exports.getCurrentMonthTransactions = async (req, res) => {
+  let response = {}
+
+  try {
+    const responseFromService = await userService.getCurrentMonthTransactions(req)
+    response.status = 200
+    response.message = 'Successfully got transactions current month data'
+    response.body = responseFromService
+  } catch (error) {
+    console.log('Something went wrong in transactionController.js')
+    response.status = 400
+    response.message = error.message
+  }
+
+  return res.status(response.status).send(response)
+}
+
+module.exports.getTransactionById = async (req, res) => {
+  let response = {}
+
+  try {
+    const responseFromService = await userService.getTransactionById(req)
+    response.status = 200
+    response.message = 'Successfully got selected transaction data'
+    response.body = responseFromService
+  } catch (error) {
+    console.log('Something went wrong in transactionController.js')
+    response.status = 400
+    response.message = error.message
+  }
+
+  return res.status(response.status).send(response)
+}
+
+module.exports.updateTransactionEditableSettings = async (req, res) => {
+  let response = {}
+
+  try {
+    const responseFromService = await userService.updateTransactionEditableSettings(req)
+    response.status = 200
+    response.message = 'Successfully updated selected transaction data'
+    response.body = responseFromService
+  } catch (error) {
+    console.log('Error in updateTransaction - transactionController.js')
     response.status = 400
     response.message = error.message
   }
